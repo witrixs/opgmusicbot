220f31df1877f6a269c5cf8190968b691e93d8ae
witrix <78809123+witrixs@users.noreply.github.com>
2026-02-28 22:34:50 +0500

    Update repository URL in script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -13,7 +13,7 @@ COMPOSE_FILE="$APP_DIR/docker-compose.yml"
 ENV_FILE="$APP_DIR/.env"
 
 # URL репозитория со скриптом и compose (ветка main)
-REPO_RAW_URL="${REPO_RAW_URL:-https://raw.githubusercontent.com/witrixs/script/main/opgmusicbot}"
+REPO_RAW_URL="${REPO_RAW_URL:-https://raw.githubusercontent.com/witrixs/opgmusicbot/main}"
 
 colorized_echo() {
   local color=$1
